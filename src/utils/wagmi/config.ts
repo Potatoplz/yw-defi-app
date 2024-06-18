f9da6fea4b16a074564ec76433e5c80c770b1c35
Yeonwoo <yw.choi@creder.biz>
2024-06-18 16:50:45 +0900

    fix: Fix dAppMetadata option error

--- src/utils/wagmi/config.ts
+++ src/utils/wagmi/config.ts
@@ -3,11 +3,23 @@ import { base, mainnet, optimism } from "wagmi/chains";
 import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
 
 const projectId = "3fbb6bba6f1de962d911bb5b5c9dba88";
+const MetaMaskOptions = {
+  dappMetadata: {
+    name: "Example Wagmi dapp",
+  },
+  infuraAPIKey: "YOUR-API-KEY",
+  // Other options.
+};
 
 export const config = createConfig({
   chains: [mainnet, optimism, base],
   ssr: true,
-  connectors: [injected(), walletConnect({ projectId }), metaMask(), safe()],
+  connectors: [
+    injected(),
+    walletConnect({ projectId }),
+    metaMask(MetaMaskOptions),
+    safe(),
+  ],
   transports: {
     [mainnet.id]: http(),
     [optimism.id]: http(),

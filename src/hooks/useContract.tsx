e2ec7d0f785ce0d2e6b2d2dae6c6587fea6a7639
Yeonwoo <yw.choi@creder.biz>
2024-06-21 16:24:47 +0900

    add: Watch for wallet network changes

--- src/hooks/useContract.tsx
+++ src/hooks/useContract.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useAccount, useWalletClient } from "wagmi";
 import { ethers } from "ethers";
-import { log } from "console";
+
+import { config } from "@/utils/wagmi/config";
+import { getChains } from "@wagmi/core";
 
 // useContract hook: Fetches the contract instance using the contract address and ABI.
 export function useContract(contractAddress: string, contractABI: any) {
-  const { address, isConnected } = useAccount();
+  const { address, isConnected, connector } = useAccount();
   const { data: walletClient } = useWalletClient();
   const [contract, setContract] = useState<ethers.Contract | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
@@ -18,6 +20,8 @@ export function useContract(contractAddress: string, contractABI: any) {
         console.log(">>> Wallet client: ", walletClient);
         console.log(">>> Wallet address: ", address);
 
+        console.log(">>> Get chains: ", getChains(config));
+
         const provider = new ethers.BrowserProvider(walletClient);
         const signer = await provider.getSigner();
         const contractInstance = new ethers.Contract(
@@ -25,6 +29,7 @@ export function useContract(contractAddress: string, contractABI: any) {
           contractABI,
           signer
         );
+        console.log(">>> Contract instance: ", contractInstance);
 
         setContract(contractInstance);
         setLoading(false);
@@ -34,5 +39,5 @@ export function useContract(contractAddress: string, contractABI: any) {
     initContract();
   }, [isConnected, walletClient, contractAddress, contractABI]);
 
-  return { contract, loading };
+  return { contract, loading, address, connector };
 }
